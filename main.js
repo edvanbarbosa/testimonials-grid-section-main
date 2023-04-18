0103c6916dbf99adc4bfb32a9dec875a4ebfc6ea
edvanbarbosa <edvabarbosa279@gmail.com>
2023-04-18 18:40:39 -0300

    finaly-commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import VanillaTilt from "./vanilla-tilt.js";
+
+
+
+VanillaTilt.init(document.querySelectorAll("section"))
+
+
+ScrollReveal().reveal('.daniel-container',{delay:250});
+ScrollReveal().reveal('.patrick-container',{delay:450})
+ScrollReveal().reveal('.jonathan-container',{delay:650})
+ScrollReveal().reveal('.jeanette-container',{delay:850})
+ScrollReveal().reveal('.kira-container',{delay:1050})
